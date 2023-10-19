0b2144453a2508d40bbc10801a636c4b97191fca
tphill <tphill760@gmail.com>
2023-10-19 13:46:46 -0400

    adding css

--- app/app/page.js
+++ app/app/page.js
@@ -1,19 +1,25 @@
-'use client'
+"use client"
 import { useState } from 'react'
 import React from "react"
 import Style from './style.css'
 
-export default function Home() {
+export default function App() {
   const {currentItem, setCurrentItem} = useState(null);
-  const onChangeHandler=(e) => {
-    console.log(e)
+  const {itemList, updateItemList} = useState([]);
+
+  const onChangeHandler = (e) => {
+    // jdh
+    console.log(e);
+    setCurrentItem(e.target.value)
   }
+
   return (
     <main className={Style.main}>
         <div className="outside-box1">
           <div className="task-box">
             <div className="input">
               <input value={currentItem} onChange={onChangeHandler} placeholder="Add a task"/>
+              <button>+</button>
             </div>
           </div>
         </div>
