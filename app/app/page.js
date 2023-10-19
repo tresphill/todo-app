32f7988679c6490412de761824f3188f8a16711d
tphill <tphill760@gmail.com>
2023-10-19 11:46:46 -0400

    onchange function

--- app/app/page.js
+++ app/app/page.js
@@ -1,13 +1,19 @@
+'use client'
+import { useState } from 'react'
 import React from "react"
 import Style from './style.css'
 
 export default function Home() {
+  const {currentItem, setCurrentItem} = useState(null);
+  const onChangeHandler=(e) => {
+    console.log(e)
+  }
   return (
     <main className={Style.main}>
         <div className="outside-box1">
           <div className="task-box">
             <div className="input">
-              <input placeholder="Add a task"/>
+              <input value={currentItem} onChange={onChangeHandler} placeholder="Add a task"/>
             </div>
           </div>
         </div>
